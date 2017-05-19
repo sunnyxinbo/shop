a16d315d239c2a1e423c1c8211d9b8409335ab55
韩励智 <hanlizhi@hotmail.com>
2017-05-19 13:04:32 +0800

    test

--- src/main/java/com/changjiang/mapper/FunctionMapper.java
+++ src/main/java/com/changjiang/mapper/FunctionMapper.java
@@ -4,8 +4,20 @@ import com.changjiang.entity.Function;
 import com.changjiang.entity.FunctionExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
 
 public interface FunctionMapper {
+	//直接获取所有功能表的数据
+	@Select("SELECT * FROM function")
+	@Results({
+		@Result(id=true,column="id",property="id"),
+		@Result(column="function_name",property="functionName"),
+		@Result(column="upper_level_id",property="upperLevelId"),
+		@Result(column="current_level",property="currentLevel")
+	})
+	List<Function> selectAllFuncton();
     long countByExample(FunctionExample example);
 
     int deleteByExample(FunctionExample example);
---
+++ src/main/java/com/changjiang/util/TreeUtil.java
@@ -0,0 +1,35 @@
+package com.changjiang.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.changjiang.entity.Function;
+
+public class TreeUtil {
+	//根据数据库中查出的数据转化为，节点集合的形式
+	public static List<Node> getListFunction(List<Function> data){
+		//初始化
+		List<Node> result=new ArrayList<>();
+		//存储被加载到节点集合中的数据
+		ArrayList<Function> temps=new ArrayList<>();
+		//第一次遍历，获取所有的顶级节点
+		for(int i=0;i<data.size();i++){
+			Function f=data.get(i);
+			Integer upperLevelId=f.getUpperLevelId();
+			//等于0为顶级节点
+			if(upperLevelId==0){
+				Node node=new Node();
+				node.setId(f.getId());
+				node.setName(f.getFunctionName());
+				node.setCurrentLevel(f.getCurrentLevel());
+				result.add(node);
+			}
+			temps.add(f);
+		}
+		//对之前被加载到节点集合的数据删除，防止重复遍历
+		for(Function f:temps){
+			data.remove(f);
+		}
+		//对
+	}
+}
